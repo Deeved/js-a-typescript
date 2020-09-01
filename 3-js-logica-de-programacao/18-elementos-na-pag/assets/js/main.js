540a034154e3984d4b7549afef4336c828b31336
Deeved Hiuston <deeved@gmail.com>
2020-09-01 11:41:55 -0300

    Resolução exercício 18 - criando elementos com laço de repetição e colocando no html

---
+++ 3-js-logica-de-programacao/18-elementos-na-pag/assets/js/main.js
@@ -0,0 +1,30 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+]
+
+const section = document.querySelector("section")
+const div = document.createElement('div')
+
+elementos.map( item => {
+    let { tag, texto } = item
+    let element = document.createElement(tag)
+    elementText = document.createTextNode(texto)
+    element.appendChild(elementText)
+    div.appendChild(element)
+})
+
+section.appendChild(div)
+
+
+/* usando o for in
+    for (const item in elementos) {
+        let { tag, texto } = elementos[item]
+        let element = document.createElement(tag)
+        elementText = document.createTextNode(texto)
+        element.appendChild(elementText)
+        div.appendChild(element)
+    }
+*/
