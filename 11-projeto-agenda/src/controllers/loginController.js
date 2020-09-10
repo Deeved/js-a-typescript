6074b2b502c2e43891295d1aab8da5f086f0c99e
Deeved Hiuston <deeved@gmail.com>
2020-09-10 17:27:03 -0300

    criando usuários 1

--- 11-projeto-agenda/src/controllers/loginController.js
+++ 11-projeto-agenda/src/controllers/loginController.js
@@ -1,3 +1,31 @@
+const Login = require('../models/LoginModel')
+const { async } = require('regenerator-runtime')
+
 exports.index = (req, res) => {
     res.render('login')
+}
+
+exports.register = async (req, res) => {
+    const login = new Login(req.body)
+    
+    try {
+        await login.register()
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save( () => {
+                res.redirect('back')
+            })
+            return
+        }
+        
+        req.flash('success', 'Seu usuário foi criado com sucesso!')
+        req.session.save( () => {
+            res.redirect('back')
+        })
+
+    } catch (e) {
+        console.log(e)
+        return res.render('404')
+    }
 }
