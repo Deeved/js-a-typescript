457252d92dec844a2d600aa22e268cccd71db224
deeved <deeved@gmail.com>
2020-09-25 19:30:28 -0300

    CRUD usuários

--- api-rest/src/controllers/UserController.js
+++ api-rest/src/controllers/UserController.js
@@ -12,6 +12,81 @@ class UserController {
       })
     }
   }
+
+  // index
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      return res.json(users)
+    } catch (error) {
+      return res.json(null)
+    }
+  }
+
+  // show
+  async show(req, res) {
+    try {
+      const { id } = req.params
+      const user = await User.findByPk(id)
+      return res.json(user)
+    } catch (error) {
+      return res.json(null)
+    }
+  }
+
+  // upadate
+  async update(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        })
+      }
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        })
+      }
+
+      const userUpdated = await user.update(req.body)
+      return res.json(userUpdated)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      })
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        })
+      }
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        })
+      }
+
+      await user.destroy()
+      return res.json(user)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      })
+    }
+  }
 }
 
 export default new UserController()
--- api-rest/src/routes/userRoutes.js
+++ api-rest/src/routes/userRoutes.js
@@ -4,13 +4,17 @@ import userController from '../controllers/UserController'
 const router = new Router()
 
 router.post('/', userController.store)
+router.get('/', userController.index)
+router.get('/:id', userController.show)
+router.put('/:id', userController.update)
+router.delete('/:id', userController.delete)
 
 export default router
 
 /*
-index -> lista todos os usuários -> get
-store/create -> cria um novo usuário -> post
-delete -> apaga um usuário -> delete
-show -> mostra um usuário -> get
-update -> atualiza um usuário -> pacth ou put
+  index -> lista todos os usuários -> get
+  store/create -> cria um novo usuário -> post
+  delete -> apaga um usuário -> delete
+  show -> mostra um usuário -> get
+  update -> atualiza um usuário -> pacth ou put
 */
